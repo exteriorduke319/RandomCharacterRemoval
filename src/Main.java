59e7a562d514bb00260334305534e3ce58a1f0f7
exteriorduke319 <yarnevictanner@gmail.com>
2023-10-19 09:20:11 -0400

    Init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import java.lang.Math;
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        boolean cont = false;
+        String contYN;
+        String word;
+        int random;
+
+        System.out.println("Enter a word or phrase:");
+        word = scan.nextLine();
+
+        if (word.length() > 1) {
+            do {
+                random = (int)(Math.random()*word.length());
+                word = word.substring(0,random) + word.substring(random +1);
+                System.out.println("Your new word/phrase is: " + word);
+                if (word.length() > 1) {
+                    System.out.println("Again?");
+                    contYN = scan.nextLine();
+                    if (contYN.equalsIgnoreCase("N")){
+                        cont = true;
+                    }
+                } else {
+                    System.out.println("Your word/phrase can't be smaller.");
+                    cont = true;
+                }
+            } while (!cont);
+        } else {
+            System.out.println("Your word/phrase is too small to continue.");
+        }
     }
 }
